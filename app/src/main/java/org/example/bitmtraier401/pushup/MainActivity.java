766f966a20e79f7bc955c898c0b60eed29deafd6
Rubuya Tabassum <rubuyatabassum09@gmail.com>
2017-03-06 21:34:27 +0600

    Initial commit

---
+++ app/src/main/java/org/example/bitmtraier401/pushup/MainActivity.java
@@ -0,0 +1,16 @@
+package org.example.bitmtraier401.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private void Testupload(){
+       int k=1+1;
+    }
+}
